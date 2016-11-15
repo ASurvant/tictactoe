08182bc207cb1e83f9665db00855acd8e0484281
Anthony Survant <anthonysurvant@gmail.com>
2016-11-14 19:09:13 -0500

    removed double piece placement within a tile

--- tictac.js
+++ tictac.js
@@ -1,21 +1,31 @@
-$(function(){
+$(function() {
   $turn = true;
+  $('.whose-move').text("someone start");
 
-  $('div').mouseenter(function(){
+  $('div').mouseenter(function() {
     $(this).css("background-color", "yellow");
   });
 
-  $('div').mouseleave(function(){
+  $('div').mouseleave(function() {
     $(this).css("background-color", "white");
   });
 
-  $('div').click(function(){
-    if($turn) {
-      $(this).append('X');
-      $turn = !$turn;
-    } else {
-      $(this).append('O');
-      $turn = !$turn;
+  $('div').click(function() {
+    if ($(this).empty) {
+      if ($turn && !$(this).hasClass('.taken')) {
+
+        $(this).append('<h5>X</h5>').addClass('.taken');
+        $turn = !$turn;
+        $('.whose-move').text("player 2's move");
+
+      } else if (!$(this).hasClass('.taken')) {
+
+        $(this).append('<h5>O</h5>').addClass('.taken');
+        $turn = !$turn;
+        $('.whose-move').text("player 1's move");
+      };
     };
   });
+
+
 });
